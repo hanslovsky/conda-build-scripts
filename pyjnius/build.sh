e90e0a8d49dd32e502cc77b6043cb1ff8bc673e3
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-03-17 08:23:45 -0400

    Set PYJNIUS_JAR environment variable

--- pyjnius/build.sh
+++ pyjnius/build.sh
@@ -7,4 +7,11 @@ python setup.py install
 
 
 cp build/pyjnius.jar $PYJNIUS_SHARE
-echo $PYJNIUS_SHARE
+
+# ensure that PYJNIUS_JAR is set correctly
+mkdir -p $PREFIX/etc/conda/activate.d
+echo 'export PYJNIUS_JAR_BACKUP=$PYJNIUS_JAR' > "$PREFIX/etc/conda/activate.d/pyjnius.sh"
+echo 'export PYJNIUS_JAR=$CONDA_PREFIX/share/pyjnius/pyjnius.jar' >> "$PREFIX/etc/conda/activate.d/pyjnius.sh"
+mkdir -p $PREFIX/etc/conda/deactivate.d
+echo 'export PYJNIUS_JAR=$PYJNIUS_JAR_BACKUP' > "$PREFIX/etc/conda/deactivate.d/pyjnius.sh"
+echo 'unset PYJNIUS_JAR_BACKUP' >> "$PREFIX/etc/conda/deactivate.d/pyjnius.sh"
