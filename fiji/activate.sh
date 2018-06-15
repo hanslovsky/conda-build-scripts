44b47d174b8c815416337632154b3584174691a7
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-15 17:35:36 -0400

    Add fiji

---
+++ fiji/activate.sh
@@ -0,0 +1,4 @@
+export FIJI_APP_DIR_CONDA_BACKUP=$FIJI_APP_DIR
+export FIJI_APP_DIR="${CONDA_PREFIX}/share/Fiji.app"
+export FIJI_JARS_CONDA_BACKUP=$FIJI_JARS
+export FIJI_JARS="${FIJI_APP_DIR}/jars/*:${FIJI_APP_DIR}/plugins/*"
---
+++ fiji/build.sh
@@ -0,0 +1,38 @@
+OS_STRING="$(uname -s)"
+
+case "${OS_STRING}" in
+    Linux*) IMAGEJ=ImageJ-linux64;;
+    Darwin*) IMAGEJ=ImageJ-macosx;;
+    *) IMAGEJ=;;
+esac
+
+if [ -e "${IMAGEJ}" ]; then
+    "Failed to build: do not understand operating system: ${OS_STRING}"
+    exit 1
+fi
+
+for CHANGE in "activate" "deactivate"
+do
+    TARGET_DIR="${CHANGE}.d"
+    FILENAME="${CHANGE}.sh"
+    mkdir -p "${PREFIX}/etc/conda/${TARGET_DIR}"
+    cp "${RECIPE_DIR}/${FILENAME}" "${PREFIX}/etc/conda/${TARGET_DIR}/${PKG_NAME}_${FILENAME}"
+done
+
+ls "${RECIPE_DIR}"
+ls "${PREFIX}"
+ls .
+
+
+mkdir -p "${PREFIX}/share"
+mv "Fiji.app" "${PREFIX}/share/Fiji.app"
+
+mkdir -p "${PREFIX}/bin"
+ln -s "${PREFIX}/share/Fiji.app/${IMAGEJ}" "${PREFIX}/bin/${IMAGEJ}"
+ln -s "${PREFIX}/bin/${IMAGEJ}" "${PREFIX}/bin/imagej"
+ln -s "${PREFIX}/bin/${IMAGEJ}" "${PREFIX}/bin/fiji"
+
+
+
+
+
