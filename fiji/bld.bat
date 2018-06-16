c6f8375e1c41af99fb62bfdef9c0be49412fafba
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-15 23:25:52 -0400

    Update remaining bash style lines to batch

--- fiji/bld.bat
+++ fiji/bld.bat
@@ -6,12 +6,12 @@ FOR %%F IN (activate deactivate) DO (
 )
 
 IF NOT EXIST %PREFIX%\share MKDIR %PREFIX%\share
-MOVE Fiji.app %PREFIX%/share/Fiji.app
+MOVE Fiji.app %PREFIX%\share\Fiji.app
 
 IF NOT EXIST %PREFIX%\bin MKDIR %PREFIX%\bin
-MKLINK "${PREFIX}/bin/${IMAGEJ}" "${PREFIX}/share/Fiji.app/${IMAGEJ}"
-MKLINK "${PREFIX}/bin/imagej" "${PREFIX}/bin/${IMAGEJ}"
-MKLINK "${PREFIX}/bin/fiji" "${PREFIX}/bin/${IMAGEJ}"
+MKLINK "%PREFIX%\bin\ImageJ-win64.exe" "%PREFIX%\share\Fiji.app\ImageJ-win64.exe"
+MKLINK "%PREFIX%\bin\imagej" "%PREFIX%\bin\ImageJ-win64.exe"
+MKLINK "%PREFIX%\bin\fiji" "%PREFIX%\bin\ImageJ-win64.exe"
 
 
 
