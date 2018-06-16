8a4c9889e018e217138b1b75f12eb1cd72aa2cc1
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-15 23:19:06 -0400

    Add windows build for fiji

---
+++ fiji/bld.bat
@@ -0,0 +1,19 @@
+setlocal EnableDelayedExpansion
+
+FOR %%F IN (activate deactivate) DO (
+    IF NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
+    COPY %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+)
+
+IF NOT EXISTS %PREFIX%\share MKDIR %PREFIX%\share
+MOVE Fiji.app %PREFIX%/share/Fiji.app
+
+IF NOT EXISTS %PREFIX%\bin MKDIR %PREFIX%\bin
+MKLINK "${PREFIX}/bin/${IMAGEJ}" "${PREFIX}/share/Fiji.app/${IMAGEJ}"
+MKLINK "${PREFIX}/bin/imagej" "${PREFIX}/bin/${IMAGEJ}"
+MKLINK "${PREFIX}/bin/fiji" "${PREFIX}/bin/${IMAGEJ}"
+
+
+
+
+
