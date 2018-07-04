e640bdbe7f4c6ea9e2a01d141cd40f7b60066672
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-03 20:36:04 -0400

    Add paintera-conversion-helper

---
+++ paintera-conversion-helper/paintera_conversion_helper/paintera_conversion_helper.py
@@ -0,0 +1,20 @@
+import jrun.jrun
+import os
+import sys
+
+repositories = {
+    'saalfeldlab'   : 'https://saalfeldlab.github.io/maven',
+    'imagej.public' : 'https://maven.imagej.net/content/groups/public'
+    }
+
+def jrun_paintera_conversion_helper():
+    repository_strings = ['-r'] + ['{}={}'.format(k, v) for (k, v) in repositories.items()]
+    endpoint           = ['org.janelia.saalfeldlab:paintera-conversion-helper:0.1.0']
+    argv               = repository_strings + endpoint + sys.argv[1:]
+
+    jrun.jrun.jrun_main(argv)
+
+if __name__ == "__main__":
+    jrun_paintera_conversion_helper()
+    
+
---
+++ paintera-conversion-helper/paintera_conversion_helper/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.command.build_py import build_py
+
+setup(
+    name='jrun-paintera-conversion-helper',
+    version='0.1.0',
+    author='Philipp Hanslovsky',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='paintera-conversion-helper',
+    url='https://github.com/saalfeldlab/paintera-conversion-helper',
+    py_modules=['paintera_conversion_helper'],
+    entry_points={
+        'console_scripts': [
+            'paintera-conversion-helper=paintera_conversion_helper:jrun_paintera_conversion_helper'
+        ]
+    },
+)
