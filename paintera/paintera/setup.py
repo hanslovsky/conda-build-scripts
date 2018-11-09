04204d779034b9d257744aab7656e355adc5fac4
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-11-09 08:16:12 -0500

    Release paintera-0.8.0 to conda

--- paintera/paintera/setup.py
+++ paintera/paintera/setup.py
@@ -3,7 +3,7 @@ from distutils.command.build_py import build_py
 
 setup(
     name='jrun-paintera',
-    version='0.7.0',
+    version='0.8.0',
     author='Philipp Hanslovsky',
     author_email='hanslovskyp@janelia.hhmi.org',
     description='paintera',
