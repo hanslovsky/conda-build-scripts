8ff0d78ed77c30883d3bee38f48ee1275f1e4b28
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-25 17:14:52 -0500

    Release paintera-0.9.2 to conda

--- paintera/paintera/setup.py
+++ paintera/paintera/setup.py
@@ -3,7 +3,7 @@ from distutils.command.build_py import build_py
 
 setup(
     name='paintera',
-    version='0.9.0',
+    version='0.9.2',
     author='Philipp Hanslovsky',
     author_email='hanslovskyp@janelia.hhmi.org',
     description='paintera',
