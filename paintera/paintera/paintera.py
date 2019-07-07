3ecfea25917abf4338808eb0a2317986d21a6930
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-07-07 09:07:31 -0400

    Release Paintera 0.15.0 to conda

--- paintera/paintera/paintera.py
+++ paintera/paintera/paintera.py
@@ -3,7 +3,7 @@ import os
 import psutil
 import sys
 
-__paintera_version__        = '0.14.1'
+__paintera_version__        = '0.15.0'
 __slf4j_version__           = '1.7.25'
 __paintera_show_container__ = '@PainteraShowContainer'
 
